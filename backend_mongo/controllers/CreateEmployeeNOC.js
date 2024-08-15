f56e40279752d25f887f63facc6bc8f11c17e8fe
Bhaski47 <bhaskarg2003@gmail.com>
2024-08-15 11:45:18 +0530

    Fixed error while submitting the employee form

--- backend_mongo/controllers/CreateEmployeeNOC.js
+++ backend_mongo/controllers/CreateEmployeeNOC.js
@@ -5,19 +5,18 @@ const CreateEmployeeNOC = async(req, res) => {
     try{
         const user = await Employee.findOne({email: req.body.email});
         if(!user) return res.status(200).send({ status_code:400,message: 'Employee not found'});
-        const { date , ...formData } = req.body;
-        const applyingDate = new Date(date);
+        const {...formData } = req.body;
 
         const getDetails = ENOCApply.findOne({email: req.body.email})
         if(getDetails){
             const result = await ENOCApply.findOneAndUpdate(
                 { email: req.body.email },
-                { ...formData, applyingDate },
+                { ...formData },
                 { new: true, upsert: true })
             return res.status(200).send({status_code:200, message:"Application Updated Successfully"});
         }
         else{
-            await ENOCApply({...formData,applyingDate}).save();
+            await ENOCApply({...formData}).save();
         }
         res.status(200).send({status_code:200,message:"Application Created Successfully"});
     }
--- backend_mongo/models/EmployeeNOCApply.js
+++ backend_mongo/models/EmployeeNOCApply.js
@@ -1,36 +1,37 @@
 const mongoose = require('mongoose');
 
-const EmployeeNOCApply = new mongoose.Schema({
-    email: {type: String, unique: true, required: true},
-    bookReturn_lang: {type: String, required: true, default: "no"},
-    gemUser_it: {type: String, required: true, default: "no"},
-    gemId_it: {type: String, default: null},
-    gemIdTransfer_it: {type: String, default: "no"},
-    applyingDate: {type: Date, required: true},
-    creditSocietyMember_society: {type: String, default: "no"},
-    creditSocietyClearance: {type: String, default: null},
-    pmsSubmission_stores: {type: String, default: "no"},
-    accessCardReturn: {type: String, default: "no"},
-    registerReturn_stores: {type: String, default: "no"},
-    itAssetsReturn_it: {type: String, default: "no"},
-    sapUser_it: {type: String, default: "no"},
-    sapId_it: {type: String, default: null},
-    residentialQuarters_hr: {type: String, default: "no"},
-    cpppUser_it: {type: String, default: "no"},
-    cpppId_it: {type: String, default: null},
-    employeeCardReturn_hr: {type: String, default: "no"},
-    nocReason: {type: String, default: "Transfer"},
-    biometricTransfer_hr: {type: String, default: "no"},
-    officeDuesDetailed_finance: {type: String, default: "no"},
-    identityCardReturn_security: {type: String, default: "no"},
-    pendingEOfficeFiles_it: {type: String, default: "no"},
-    itValidated: {type: String, default: "Pending"},
-    hrValidated: {type: String, default: "Pending"},
-    securityValidated: {type: String, default: "Pending"},
-    estateValidated: {type: String, default: "Pending"},
-    societyValidated: {type: String, default: "Pending"}
+const EmployeeNOCApplySchema = new mongoose.Schema({
+    email: { type: String, unique: true, required: true },
+    bookReturn_lang: { type: String, required: true, default: "no" },
+    gemUser_it: { type: String, required: true, default: "no" },
+    gemId_it: { type: String, default: null },
+    gemIdTransfer_it: { type: String, default: "no" },
+    applyingDate: { type: Date, required: true },
+    creditSocietyMember_society: { type: String, default: "no" },
+    creditSocietyClearance: { type: String, default: null },
+    pmsSubmission_stores: { type: String, default: "no" },
+    accessCardReturn: { type: String, default: "no" },
+    registerReturn_stores: { type: String, default: "no" },
+    itAssetsReturn_it: { type: String, default: "no" },
+    sapUser_it: { type: String, default: "no" },
+    sapId_it: { type: String, default: null },
+    residentialQuarters_hr: { type: String, default: "no" },
+    cpppUser_it: { type: String, default: "no" },
+    cpppId_it: { type: String, default: null },
+    employeeCardReturn_hr: { type: String, default: "no" },
+    nocReason: { type: String, default: "Transfer" },
+    societyId_society:{type: String,default:null},
+    biometricTransfer_hr: { type: String, default: "no" },
+    officeDuesDetailed_finance: { type: String, default: "no" },
+    identityCardReturn_security: { type: String, default: "no" },
+    pendingEOfficeFiles_it: { type: String, default: "no" },
+    itValidated: { type: String, default: "Pending" },
+    hrValidated: { type: String, default: "Pending" },
+    securityValidated: { type: String, default: "Pending" },
+    estateValidated: { type: String, default: "Pending" },
+    societyValidated: { type: String, default: "Pending" }
 });
 
-const ENOCApply = mongoose.model("ENOCApply", EmployeeNOCApply);
+const ENOCApply = mongoose.model("ENOCApply", EmployeeNOCApplySchema);
 
-module.exports = {ENOCApply};
+module.exports = { ENOCApply };
